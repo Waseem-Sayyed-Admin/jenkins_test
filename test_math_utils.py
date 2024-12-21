77b80516df0e25bdc6a335744f027138575a34af
daniyalcodesaltnix <q.daniyal@altnix.com>
2024-12-21 18:10:52 +0530

    first commit

---
+++ test_math_utils.py
@@ -0,0 +1,20 @@
+# test_math_utils.py
+import unittest
+from math_utils import add
+
+class TestMathUtils(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(3, 5), 8)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-3, -5), -8)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-3, 5), 2)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 5), 5)
+        self.assertEqual(add(3, 0), 3)
+
+if __name__ == "__main__":
+    unittest.main()
