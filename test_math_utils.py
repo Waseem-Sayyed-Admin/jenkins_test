65c305edc3d79466981cce1ab4d5819636a15222
Waseem <waseemawst@gmail.com>
2025-08-07 22:05:47 +0530

    Update test_math_utils_v4.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -5,7 +5,7 @@ def add(a, b):
 
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
-        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, 5), 3)
 
     def test_add_negative_numbers(self):
         self.assertEqual(add(-1, -2), -3)
