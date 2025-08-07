ac2a16c43a8d9533392ee11f5b99178140842c4f
Waseem <waseemawst@gmail.com>
2025-08-07 21:47:09 +0530

    Update test_math_utils_v3.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,20 +1,18 @@
-# test_math_utils.py
 import unittest
-from math_utils import add
 
-class TestMathUtils(unittest.TestCase):
+def add(a, b):
+    return a + b
+
+class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
-        self.assertEqual(add(3, 7), 8)
+        self.assertEqual(add(1, 2), 3)
 
     def test_add_negative_numbers(self):
-        self.assertEqual(add(-3, -5), -8)
+        self.assertEqual(add(-1, -2), -3)
 
     def test_add_mixed_numbers(self):
-        self.assertEqual(add(-3, 5), 2)
+        self.assertEqual(add(1, -2), -1)
+        self.assertEqual(add(-1, 2), 1)
 
-    def test_add_zero(self):
-        self.assertEqual(add(0, 5), 5)
-        self.assertEqual(add(3, 0), 3)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
